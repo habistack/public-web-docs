0aef30b4aebe4246bd899c91627482266ee36a59
Matt <matt.jackson@fathym.com>
2022-06-30 11:36:19 -0600

    removing svg test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ module.exports = {
       },
       items: [
         {
-          href: 'https://www.fathym.com/forecast',
+          to: 'https://www.fathym.com/forecast',
           label: 'Home',
           position: 'left',
           target: '_top',
